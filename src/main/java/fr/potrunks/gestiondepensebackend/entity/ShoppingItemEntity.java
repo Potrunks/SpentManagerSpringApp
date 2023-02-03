01ebefbd0d7be5eb87ba7fe9d47a6c044d16525b
Potrunks <potrunks@hotmail.com>
2023-02-04 00:21:07 +0100

    new entity #43

---
+++ src/main/java/fr/potrunks/gestiondepensebackend/entity/ShoppingItemEntity.java
@@ -0,0 +1,29 @@
+package fr.potrunks.gestiondepensebackend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "shopping_item")
+public class ShoppingItemEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "isBought")
+    private Boolean isBought;
+}

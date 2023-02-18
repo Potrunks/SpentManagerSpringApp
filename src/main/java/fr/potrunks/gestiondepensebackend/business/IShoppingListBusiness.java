3e49efacfd207e3b86b15016971f2da7319e6a3b
Potrunks <potrunks@hotmail.com>
2023-02-18 11:16:02 +0100

    remove not used import

--- src/main/java/fr/potrunks/gestiondepensebackend/business/IShoppingListBusiness.java
+++ src/main/java/fr/potrunks/gestiondepensebackend/business/IShoppingListBusiness.java
@@ -1,6 +1,5 @@
 package fr.potrunks.gestiondepensebackend.business;
 
-import fr.potrunks.gestiondepensebackend.model.Report;
 import fr.potrunks.gestiondepensebackend.model.ShoppingItem;
 import fr.potrunks.gestiondepensebackend.model.ShoppingItemReport;
 
--- src/main/java/fr/potrunks/gestiondepensebackend/model/ShoppingItemReport.java
+++ src/main/java/fr/potrunks/gestiondepensebackend/model/ShoppingItemReport.java
@@ -1,8 +1,6 @@
 package fr.potrunks.gestiondepensebackend.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
